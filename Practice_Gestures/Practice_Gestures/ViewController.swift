fb487893310c279708da1799d25ea442a120131b
Koryagin Sergey <>
2021-07-09 20:18:48 +0300

    changed rotation and pinch

--- Practice_Gestures/Practice_Gestures/ViewController.swift
+++ Practice_Gestures/Practice_Gestures/ViewController.swift
@@ -19,8 +19,11 @@ class ViewController: UIViewController {
     let rotationGestureRecognizer = UIRotationGestureRecognizer()
     let tapGestureRecognizer = UITapGestureRecognizer()
     var panGestureAnchorPoint: CGPoint?
-    var scale: CGFloat = 0.0
-    var rotate: CGFloat = 1.0
+    var scale: CGFloat = 1.0
+    var rotate: CGFloat = 0.0
+    
+    var pinchAnchorScale: CGFloat?
+    var rotateAnchorRotation: CGFloat?
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -51,12 +54,9 @@ class ViewController: UIViewController {
         case .changed:
             guard let panGestureAnchorPoint = panGestureAnchorPoint else {return}
             let gesturePoint = gestureRecognizer.location(in: view)
-            
             centreXConstraint.constant += gesturePoint.x - panGestureAnchorPoint.x
             centreYConstraint.constant += gesturePoint.y - panGestureAnchorPoint.y
-            
             self.panGestureAnchorPoint = gesturePoint
-            
         case .cancelled, .ended:
             panGestureAnchorPoint = nil
         case .failed, .possible:
@@ -67,25 +67,39 @@ class ViewController: UIViewController {
     }
     
     @objc func handlePinchGesture(_ gestureRecognizer: UIPinchGestureRecognizer) {
-        
+       
         switch gestureRecognizer.state {
-        case .changed, .began:
-            scale = gestureRecognizer.scale
-            redView.transform = CGAffineTransform.identity.scaledBy(x: scale, y: scale).rotated(by: rotate)
-        default:
-            break
-        }
+                case .began:
+                    pinchAnchorScale = gestureRecognizer.scale
+                case .changed:
+                    guard let pinchAnchorScale = pinchAnchorScale else { assert(false); return }
+                    let gestureScale = gestureRecognizer.scale
+                    scale += gestureScale - pinchAnchorScale
+                    self.pinchAnchorScale = gestureScale
+                    redView.transform = CGAffineTransform.identity.scaledBy(x: scale, y: scale).rotated(by: rotate)
+                case .cancelled, .ended:
+                    pinchAnchorScale = nil
+                default:
+                    break
+            }
     }
     
     @objc func handleRotationGesture(_ gestureRecognizer: UIRotationGestureRecognizer) {
         
         switch gestureRecognizer.state {
-        case .changed, .began:
-            rotate = gestureRecognizer.rotation
-            redView.transform = CGAffineTransform.identity.scaledBy(x: scale, y: scale).rotated(by: rotate)
-        default:
-            break
-        }
+                case .began:
+                    rotateAnchorRotation = gestureRecognizer.rotation
+                case .changed:
+                    guard let rotateAnchorRotation = rotateAnchorRotation else { assert(false); return }
+                    let gestureRotation = gestureRecognizer.rotation
+                    rotate += gestureRotation - rotateAnchorRotation
+                    self.rotateAnchorRotation = gestureRotation
+                    redView.transform = CGAffineTransform.identity.scaledBy(x: scale, y: scale).rotated(by: rotate)
+                case .cancelled, .ended:
+                    rotateAnchorRotation = nil
+                default:
+                    break
+            }
     }
     
     @objc func handleTapGesture(_ gestureRecognizer: UITapGestureRecognizer) {
